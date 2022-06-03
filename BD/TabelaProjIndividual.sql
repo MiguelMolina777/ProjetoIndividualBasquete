cc9f93d17188b92ba564560bee583c5e57764e4d
MiguelMolina777 <miguel.molina@sptech.school>
2022-06-02 23:04:29 -0300

    minor changes

--- BD/TabelaProjIndividual.sql
+++ BD/TabelaProjIndividual.sql
@@ -1,17 +1,6 @@
 CREATE DATABASE basquete;
 use basquete;
 
-CREATE TABLE jogador (
-idJogador int primary key auto_increment,
-nome varchar(45),
-email varchar(45),
-senha varchar(45),
-estado char(2),
-cidade varchar(45),
-fkQuadra int,
-foreign key (fkQuadra) references Quadra(idQuadra)
-);
-
 CREATE TABLE Quadra (
 idQuadra int primary key auto_increment,
 estado char(2),
@@ -19,11 +8,20 @@ cidade varchar(45),
 endereco varchar(45)
 );
 
+CREATE TABLE usuario (
+idUsuario int primary key auto_increment,
+nome varchar(45),
+email varchar(45),
+fkQuadra int,
+foreign key (fkQuadra) references Quadra(idQuadra),
+senha varchar(45)
+);
+
 insert into Quadra values 
 (null, 'SP', 'São Caetano do Sul', 'Espaço Verde Chico Mendes');
 
-insert into Jogador values
-(null, 'Miguel', 'miguel@email.com', '123', 'SP', 'São Bernardo do Campo', 1);
+insert into usuario values
+(null, 'Miguel', 'miguel@email.com', 1, '123');
 
 select * from quadra 
-join jogador on jogador.fkQuadra = Quadra.idQuadra; 
+join usuario on usuario.fkQuadra = Quadra.idQuadra; 
--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -64,6 +64,7 @@ function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
     var email = req.body.emailServer;
+    var quadra = req.body.quadraServer
     var senha = req.body.senhaServer;
 
     // Faça as validações dos valores
@@ -74,9 +75,9 @@ function cadastrar(req, res) {
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     } else {
-        
+        console.log('fsadsads')
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, email, quadra, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
